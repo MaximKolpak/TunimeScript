1ad90d98e41b600a00b867e934d3b19fa759d3f6
Maxim Kolpakov <max.kolpakov.st@gmail.com>
2022-02-14 23:51:58 +0200

    Adding local variables and setup header

---
+++ tunime.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Tunime Script
+// @namespace    UserScripts
+// @version      0.1
+// @description  helps to maintain a list of watched TV shows, with a nice visual part
+// @author       Anoncer (https://github.com/MaximKolpak)
+// @match        https://yummyanime.club/*
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @icon         https://www.google.com/s2/favicons?domain=yummyanime.club
+// ==/UserScript==
+
+(function(){
+'use strict';
+    const LinksWebAuth = "https://shikimori.one/oauth/authorize?client_id=EKv75uNamao_d3uzFREIfo71l6cpyG2IEUIpBxFgcAM&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=user_rates+comments+topics";
+    const user_agent = "Tunime";
+    const client_id = "EKv75uNamao_d3uzFREIfo71l6cpyG2IEUIpBxFgcAM";
+	const client_secret = "WKDClcJlc3grYpBWDbxqQyAFEW0SquPgrvTdXeAfhds";
+})();
